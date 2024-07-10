bb39854d2bed43de67dff68a29c53275757e04ac
Shivaraj Shetty <shivarajshetty635@gmail.com>
2024-07-10 11:48:30 +0530

    Updated index.js

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 const express = require("express");
 const userRouter = require('./routes/user');
 const connectMongodb = require('./connection');
+// require('dotenv').config();
 
 const app = express();
-const port = 8000;
+const port = process.env.PORT || 8000;
 const url = 'mongodb://127.0.0.1:27017/MeanJS';
 
 
 //connection
 connectMongodb(url);
-
-//Schema
-//Model
 
 app.use(express.urlencoded({ extended: false }));
 app.use('/users', userRouter);
