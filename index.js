dbd40f25856478bc97d701f4bcdef082f7341917
Shivaraj Shetty <shivarajshetty635@gmail.com>
2024-07-10 12:03:48 +0530

    Updated the Port connection

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const userRouter = require('./routes/user');
 const connectMongodb = require('./connection');
-// require('dotenv').config();
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 8000;
